b284b4c684e4588d52b9481cc56cda219ac3d0d4
mucnjakf <crofilix@gmail.com>
2026-07-17 02:41:18 +0200

    Refactor DeleteCategoryDialog to use a new reusable Dialog component for improved consistency and maintainability

--- app/web/OrderPoint.Admin/Features/Categories/Dialogs/DeleteCategoryDialog.razor.cs
+++ app/web/OrderPoint.Admin/Features/Categories/Dialogs/DeleteCategoryDialog.razor.cs
@@ -1,23 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
-using MudBlazor;
 
 namespace OrderPoint.Admin.Features.Categories.Dialogs;
 
 public sealed partial class DeleteCategoryDialog
 {
-    [CascadingParameter]
-    private IMudDialogInstance MudDialogInstance { get; set; } = null!;
-
     [Parameter]
     public string CategoryName { get; set; } = null!;
-
-    private void Confirm()
-    {
-        MudDialogInstance.Close(DialogResult.Ok(true));
-    }
-
-    private void Cancel()
-    {
-        MudDialogInstance.Cancel();
-    }
 }
