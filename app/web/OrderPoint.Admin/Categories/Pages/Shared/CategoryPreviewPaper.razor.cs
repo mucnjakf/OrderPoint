ba4a796c83cab162d5c39c59823031be2725718b
mucnjakf <crofilix@gmail.com>
2026-07-17 16:29:23 +0200

    Add CreateCategoryAlert and UpdateCategoryDeletePaper components; refactor CategoryPreviewPaper for improved category display

---
+++ app/web/OrderPoint.Admin/Categories/Pages/Shared/CategoryPreviewPaper.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using OrderPoint.Admin.Categories.Enumerations;
+
+namespace OrderPoint.Admin.Categories.Pages.Shared;
+
+public sealed partial class CategoryPreviewPaper
+{
+    [Parameter]
+    [EditorRequired]
+    public string Name { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public string Description { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public CategoryStatus Status { get; set; }
+
+    [Parameter]
+    public string NamePlaceholder { get; set; } = string.Empty;
+
+    [Parameter]
+    public string DescriptionPlaceholder { get; set; } = string.Empty;
+}
---
+++ app/web/OrderPoint.Admin/Categories/Pages/Update/Components/UpdateCategoryDeletePaper.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OrderPoint.Admin.Categories.Pages.Update.Components;
+
+public sealed partial class UpdateCategoryDeletePaper
+{
+    [Parameter]
+    [EditorRequired]
+    public bool CategoryContainsItems { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public EventCallback OnDeleteClick { get; set; }
+}
